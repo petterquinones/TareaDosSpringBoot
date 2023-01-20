e73b635972bbc93fbb4c0f3e75fef270740eb466
petterquinones <pedro.quinones@ias.com.co>
2023-01-19 23:02:19 -0500

    Declaración de TeacherDTO

---
+++ src/main/java/com/example/tareaDos/dto/TeacherDTO.java
@@ -0,0 +1,43 @@
+package com.example.tareaDos.dto;
+
+public class TeacherDTO {
+    private Integer identification;
+    private String name;
+    private String lastName;
+    private Integer age;
+
+    public TeacherDTO() {
+    }
+
+    public Integer getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(Integer identification) {
+        this.identification = identification;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
